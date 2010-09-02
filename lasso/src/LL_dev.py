0890def4280c42d8def4e0d459996abf123a80e3
Tim Kretschmer <xbigtk13x@gmail.com>
2010-09-02 18:18:40 +0000

    - Removed _dP debugging function from LL_dev - Implemented python logging module usage to replace _dP
    git-svn-id: http://lyricslasso.googlecode.com/svn/trunk@39 80444ff8-1e8f-11df-ba46-2fd12f9fb1ef

--- lasso/src/LL_dev.py
+++ lasso/src/LL_dev.py
@@ -1,7 +1,28 @@
-#These are development specific variables and functions
+#Development specific functions (logging, debugging, etc)
+import logging
 
 #This is a global trigger that tells the program whether or not it should be verbose
 DEV_MODE = 1
+
+###################
+#Sets up the logger
+# To use in any file call as follows
+#
+#    import LL_dev
+#    _L = LL_dev.logger
+#    _dP = LL_dev._dP
+#    _L.debug("Example debugging message")
+#
+###################
+MIN_LOG_LEVEL = logging.DEBUG
+LOG_FILENAME = 'll.log'
+logging.basicConfig()
+logger = logging.getLogger("LyricsLasso")
+logger.setLevel(MIN_LOG_LEVEL)
+fileHandler = logging.FileHandler(LOG_FILENAME)
+logFormat = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+fileHandler.setFormatter(logFormat)
+logger.addHandler(fileHandler)
 
 #Only prints to the console if development mode is on
 def _dP(a):
