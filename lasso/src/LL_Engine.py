09fc49c09b61af8ba4a0df13091ca1f629491581
Tim Kretschmer <xbigtk13x@gmail.com>
2010-09-02 23:36:14 +0000

    Added fault tolerance through try...except statements and log generation
    git-svn-id: http://lyricslasso.googlecode.com/svn/trunk@45 80444ff8-1e8f-11df-ba46-2fd12f9fb1ef

--- lasso/src/LL_Engine.py
+++ lasso/src/LL_Engine.py
@@ -19,69 +19,85 @@ class Engine():
     #Actually handles the writing of tags
     #Saves information in iTunes compatible format
     def WriteLyrics(self,lyrString,curMP3):
-        _L.debug("Writing lyrics...")
-        curMP3[u"USLT::'eng'"] = id3.USLT()
-        curMP3[u"USLT::'eng'"].text = lyrString
-        curMP3[u"USLT::'eng'"].encoding=1
-        curMP3[u"USLT::'eng'"].lang = 'eng'
-        curMP3[u"USLT::'eng'"].desc=u''
-        curMP3.save()
-        _L.debug("ALL FINISHED!")
+        try:
+            _L.debug("Writing lyrics...")
+            curMP3[u"USLT::'eng'"] = id3.USLT()
+            curMP3[u"USLT::'eng'"].text = lyrString
+            curMP3[u"USLT::'eng'"].encoding=1
+            curMP3[u"USLT::'eng'"].lang = 'eng'
+            curMP3[u"USLT::'eng'"].desc=u''
+            curMP3.save()
+            _L.debug("ALL FINISHED!")
+        except Exception,e:
+            _L.error("Writing tag process encountered an error")
+            _L.error("EXCEPTION:"+e)
+            pass
+            pass
         
     #This is a built in query service through 'lyricsfly'
     #Plugin based architecture (in the future) will provide
     #a way to get around copyright restrictions and service
     #outages with a single provider
     def getData(self,curMP3):
-        #The strings inside brackets are the ID tags defined by 'mutagen.id3'
-        #See doc for those modules to learn about other tags available
-        _L.debug("Querying database. Arist is '"+str(curMP3['TPE2'])+"' and Title is '"+str(curMP3['TIT2'])+"'...")
-        connection = httplib.HTTPConnection("api.lyricsfly.com")
-        #This if the format to use lyricsfly
-        connection.request("GET","/api/api.php?i=" + apiKey + "&a="+str(curMP3['TPE2'])+"&t="+str(curMP3['TIT2'])+"")
-        response = connection.getresponse()
-        if(response.status is 200):
-            _L.debug("QUERY WAS SUCCESSFULLY HANDLED BY THE SITE!")
-            data = response.read()
-            connection.close()
-        else:
-            data = "DEAD CONNECTION!"
-        return data
+        try:
+            #The strings inside brackets are the ID tags defined by 'mutagen.id3'
+            #See doc for those modules to learn about other tags available
+            _L.debug("Querying database. Arist is '"+str(curMP3['TPE2'])+"' and Title is '"+str(curMP3['TIT2'])+"'...")
+            connection = httplib.HTTPConnection("api.lyricsfly.com")
+            #This if the format to use lyricsfly
+            connection.request("GET","/api/api.php?i=" + apiKey + "&a="+str(curMP3['TPE2'])+"&t="+str(curMP3['TIT2'])+"")
+            response = connection.getresponse()
+            if(response.status is 200):
+                _L.debug("QUERY WAS SUCCESSFULLY HANDLED BY THE SITE!")
+                data = response.read()
+                connection.close()
+            else:
+                data = "DEAD CONNECTION!"
+            return data
+        except Exception, e:
+            _L.error("Retrieving lyrics tag information from the MP3 failed")
+            _L.error("EXCEPTION:"+str(e))
+            pass
     
     #This function looks up the lyrics for a single track
     #Then it embeds those lyrics into the passed MP3 file
     def EmbedLyrics(self,curMP3):
-        #Query the database and store resulting repsonse in an object
-        data = self.getData(curMP3)
-        #Used to fix IP throttle limitations
-        slpTime = 5
-        #Basically, just keep waiting longer if your IP is being throttled
-        while(data.find("Query too soon!")!=-1):
-            _L.debug("YOU QUERIED TOO QUICKLY!\nWaiting "+str(slpTime)+" seconds and then trying again...")
-            time.sleep(slpTime)
-            data = self.getData()
-            slpTime+=1
-        
-        #This section strips the extra data provided
-        #By the lyricsfly query and formats everything to look pretty =)
-        _L.debug("Stripping uneeded material from the query response...")
-        lyrString = data[data.find("<tx>")+4:data.find("</tx>")-4]
-        lyrString = lyrString.replace("[br]","")
-        lyrString = lyrString.replace("[... *** Your access is restricted to 30% of content. Please get permanent user ID key for 100% at lyricsfly.com/api/ *** ","")
-        lyrString = lyrString.replace("Lyrics delivered by lyricsfly.com ","\n(Lyrics delivered by lyricsfly.com) ")
-        lyrString = lyrString.replace("\n\n","\n")
-        #Writing newly found lyrics data to the MP3 in iTunes compatible format
-        self.WriteLyrics(lyrString,curMP3)
+        try:
+            #Query the database and store resulting repsonse in an object
+            data = self.getData(curMP3)
+            #Used to fix IP throttle limitations
+            slpTime = 5
+            #Basically, just keep waiting longer if your IP is being throttled
+            while(data.find("Query too soon!")!=-1):
+                _L.debug("YOU QUERIED TOO QUICKLY!\nWaiting "+str(slpTime)+" seconds and then trying again...")
+                time.sleep(slpTime)
+                data = self.getData()
+                slpTime+=1
+            
+            #This section strips the extra data provided
+            #By the lyricsfly query and formats everything to look pretty =)
+            _L.debug("Stripping uneeded material from the query response...")
+            lyrString = data[data.find("<tx>")+4:data.find("</tx>")-4]
+            lyrString = lyrString.replace("[br]","")
+            lyrString = lyrString.replace("[... *** Your access is restricted to 30% of content. Please get permanent user ID key for 100% at lyricsfly.com/api/ *** ","")
+            lyrString = lyrString.replace("Lyrics delivered by lyricsfly.com ","\n(Lyrics delivered by lyricsfly.com) ")
+            lyrString = lyrString.replace("\n\n","\n")
+            #Writing newly found lyrics data to the MP3 in iTunes compatible format
+            self.WriteLyrics(lyrString,curMP3)
+        except Exception,e:
+            _L.error("An engine error occurred while attempting to embed the lyrics tag")
+            _L.error("EXCEPTION: "+str(e))
+            pass
     
     #Remove lyrics from the MP3 file
     def StripLyrics(self,curMP3):
         _L.debug('Stripping lyrics...')
         self.WriteLyrics("",curMP3)
     
-    #Prevents bad access of a non-existant lyrics tag
+    #Prevents bad access of a non-existent lyrics tag
     def CreateLyricsTag(self,curMP3):
         curLyr="Empty"
-        #This protects us from reading a non-existance key entry in the ID3 tag
+        #This protects us from reading a non-existence key entry in the ID3 tag
         while(curLyr=="Empty"):
             try:
                 curLyr = curMP3[u"USLT::'eng'"].text
@@ -90,7 +106,7 @@ class Engine():
                 curLyr = curMP3[u"USLT::'eng'"] = id3.USLT()
         return curLyr
     
-    #Retrives the lyrics tag stored in the current MP3 file
+    #Retrieves the lyrics tag stored in the current MP3 file
     def GetLyrics(self,curMP3):
         self.CreateLyricsTag(curMP3)
         return curMP3[u"USLT::'eng'"].text
@@ -100,7 +116,7 @@ class Engine():
         curLyr = self.CreateLyricsTag(curMP3)
         _L.debug('Checking MP3s lyrics status...')
         if(curLyr == "" or self.GetLyrics(curMP3)==id3.USLT().text):
-            _L.debug("Lyrics don't!")
+            _L.debug("Lyrics don't exist in the current MP3 file!")
             return "No"
         else:
             _L.debug("Lyrics exist!")
