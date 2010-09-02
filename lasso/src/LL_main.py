1d6a9d6630b4b0372744e1cae7239180fdf57f60
Tim Kretschmer <xbigtk13x@gmail.com>
2010-09-02 18:20:16 +0000

    - Changed the explanation of why we use LL_main.py
    git-svn-id: http://lyricslasso.googlecode.com/svn/trunk@41 80444ff8-1e8f-11df-ba46-2fd12f9fb1ef

--- lasso/src/LL_main.py
+++ lasso/src/LL_main.py
@@ -1,7 +1,5 @@
 #This imports the GUI and launches an instance of it
-#Handling the program in this way allows for modularization
-#as the GUI utilizes the Engine multiple times (creates space for
-#multi-threading in the future) 
+#Handling the program in this way allows for better modularization
 
 import LL_GUI
 
